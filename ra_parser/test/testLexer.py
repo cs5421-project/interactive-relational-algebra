06880977e29bd771fdec7043f19c7bc5e126ba02
Sourabh Jaiswal <sourabhrj31@gmail.com>
2023-03-11 18:44:05 +0800

    ✨ add: Lexer for RA

---
+++ ra_parser/test/testLexer.py
@@ -0,0 +1,43 @@
+
+# setting path
+import sys
+sys.path.append('..')
+from Lexer import Lexer, display_tokens, Token, TokenType
+
+def main():
+    lexer: Lexer = Lexer()
+    # Test 1
+    input = "σa=10(R)"
+    tokens = lexer.tokenize(input)
+    expected = [Token("σ", TokenType.SELECT), Token("a", TokenType.IDENT), Token("=", TokenType.EQUALS), Token(
+        "10", TokenType.DIGIT), Token("(", TokenType.OPEN_PARENTHESIS), Token("R", TokenType.IDENT), Token(")", TokenType.CLOSED_PARENTHESIS)]
+    print("TEST 1: " + input + " ", end="")
+    if (tokens == expected):
+        print("SUCCESS")
+    else:
+        print("FAILED")
+
+    # Test 2
+    input = "σa1=10(R22)"
+    tokens = lexer.tokenize(input)
+    expected = [Token("σ", TokenType.SELECT), Token("a1", TokenType.IDENT), Token("=", TokenType.EQUALS), Token(
+        "10", TokenType.DIGIT), Token("(", TokenType.OPEN_PARENTHESIS), Token("R22", TokenType.IDENT), Token(")", TokenType.CLOSED_PARENTHESIS)]
+    print("TEST 2: " + input + " ", end="")
+    if (tokens == expected):
+        print("SUCCESS")
+    else:
+        print("FAILED")
+
+    # Test 3
+    input = "RTable ⋈ STable"
+    tokens=lexer.tokenize(input)
+    expected = [Token("RTable", TokenType.IDENT), Token("⋈", TokenType.NATURAL_JOIN), Token("STable", TokenType.IDENT)]
+    print("TEST 3: " + input + " ", end="")
+    if (tokens == expected):
+        print("SUCCESS")
+    else:
+        print("FAILED")
+
+
+if __name__ == "__main__":
+    main()
