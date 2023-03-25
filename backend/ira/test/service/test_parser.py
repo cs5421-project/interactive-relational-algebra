b01da869cce751175d8a1a2e0edac49c9eb051e1
Sourabh Jaiswal <sourabhrj31@gmail.com>
2023-03-25 18:35:23 +0800

    ✨ add: anti_join

--- backend/ira/test/service/test_parser.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from ira.service.lexer import Lexer, Token, TokenType
-from ira.service.parser import Parser
-
-
-class ParserTestCase(TestCase):
-    def setUp(self):
-        self.lexer: Lexer = Lexer()
-        self.parser: Parser = Parser()
