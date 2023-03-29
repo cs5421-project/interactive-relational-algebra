5759063405b01fedd2e7630ede3bb19f36f44e33
Kalai <ssokalai@gmail.com>
2023-03-30 00:22:05 +0800

    fix(backend-transformer): Wrong query with complex right operand  + WIP for Anti join

--- backend/ira/service/transformer.py
+++ backend/ira/service/transformer.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from ira.constants import TOKEN_TYPE_OPERATORS
+from ira.constants import TOKEN_TYPE_OPERATORS, TOKEN_TYPE_TO_UNARY_OPERATOR, TOKEN_TYPE_TO_BINARY_OPERATOR
 from ira.enum.token_type import TokenType
 from ira.model.attributes import Attributes
 from ira.model.query import Query
@@ -17,6 +17,7 @@ def get_n_join_queries(join_type):
 QUERY_MAPPER = {TokenType.SELECT: "select * from {{}} where {conditions};",
                 TokenType.PROJECTION: "select distinct {column_names} from {{}};",
                 TokenType.NATURAL_JOIN: "select * from {} natural join {};",
+                # TODO: Add another variant with where clause to support conditional join
                 TokenType.IDENT: "select * from {table_name};",
                 TokenType.CARTESIAN: "select * from {} cross join {};",
                 TokenType.UNION: "{} union {}",
@@ -24,7 +25,13 @@ QUERY_MAPPER = {TokenType.SELECT: "select * from {{}} where {conditions};",
                 TokenType.DIFFERENCE: "{} except {}",
                 TokenType.LEFT_JOIN: get_n_join_queries("left"),
                 TokenType.RIGHT_JOIN: get_n_join_queries("right"),
-                TokenType.FULL_JOIN: get_n_join_queries("full")}
+                TokenType.FULL_JOIN: get_n_join_queries("full"),
+                TokenType.ANTI_JOIN: ("select * from {left_table_name} natural left join {right_table_name} where ("
+                                      "select column_name from"
+                                      "information_schema.columns where table_name={left_table_name} and column_name "
+                                      "in (select column_name from information_schema.columns where table_name={"
+                                      "right_table_name});",
+                                      "select * from {} left join {} on {conditions} where {null_conditions}")}
 
 
 def transform(parsed_postfix_tokens: List[Token]) -> Query:
@@ -77,14 +84,14 @@ def transform(parsed_postfix_tokens: List[Token]) -> Query:
                                     TokenType.INTERSECTION, TokenType.DIFFERENCE):
 
             query = QUERY_MAPPER[current_token.type]
-            is_certain_join = current_token.type in(TokenType.LEFT_JOIN,
-                                                                  TokenType.RIGHT_JOIN,
-                                                                  TokenType.FULL_JOIN)
+            is_certain_join = current_token.type in (TokenType.LEFT_JOIN,
+                                                     TokenType.RIGHT_JOIN,
+                                                     TokenType.FULL_JOIN)
             if current_token.attributes and is_certain_join:
                 # If certain join operator has attributes, it implies that it is a type of conditional/equi join
                 conditions = sanitise(current_token.attributes, current_token.type)
                 query = query[-1].format("{}", "{}", conditions=conditions)
-            elif is_certain_join :
+            elif is_certain_join:
                 query = query[0]
             query_stack.append((query, current_token))
 
@@ -112,6 +119,7 @@ def generate_query(query_stack):
                             .format(number_of_subquery=number_of_subquery,
                                     column_name=column_name)
                         stored_query = stored_query.replace('"{}"'.format(column_name), alias_prefixed_column_name)
+                query = stored_query.format(query)
         return Query(query)
     else:
         raise Exception("Empty relational algebra given")
@@ -123,17 +131,29 @@ def process_query_stack(query_stack, level, current_query):
     index = upcoming_operator_and_index[-1] if upcoming_operator_and_index else None
     operator_token = upcoming_operator_and_index[0] if upcoming_operator_and_index else None
     if is_alias_not_needed(index, operator_token, query_stack, upcoming_operator_and_index, None):
-        # For top level union, intersect or difference operation were operands don't involve subqueries
+        # For top level union, intersect or difference operation where operands don't involve subqueries
         parent_query, _ = query_stack.pop()
         return parent_query.format(current_query.rstrip(';'), stored_query), None
     if stored_query is None:
         # stored_query is None for an identifier token dealing with binary op.
         parent_query, _ = query_stack.pop()
         if is_alias_not_needed(index, operator_token, query_stack, upcoming_operator_and_index, None):
+            # Fetches index of the upcoming binary operator and then relevant query, insert the current query as
+            #  an operand to it, and update current query as stored_query + parent_query
+            binary_token, index = get_upcoming_operator_token(query_stack, True)
+            if binary_token:
+                query_stack[index] = (query_stack[index][0].format(current_query, "{}"), query_stack[index][-1])
+                return parent_query.format(stored_token.value), stored_query
+
             parent_query, _ = query_stack.pop()
-            return parent_query.format(current_query.rstrip(';'), stored_query), None
+            return parent_query.format(current_query.rstrip(';'), stored_query), stored_query
+
         # Do not need an alias if it is a top level query
         level = get_level(query_stack, level)
+        binary_token, index = get_upcoming_operator_token(query_stack, True)
+        if binary_token:
+            query_stack[index] = (query_stack[index][0].format(current_query, "{}"), query_stack[index][-1])
+            return get_query_with_alias(parent_query.format(stored_token.value).rstrip(';'), level), stored_query
         query = get_query_with_alias(parent_query.format(current_query, stored_token.value), level)
     else:
         # Unary operation
@@ -146,16 +166,20 @@ def process_query_stack(query_stack, level, current_query):
 
 def is_alias_not_needed(index, operator_token, query_stack, upcoming_operator_and_index, stored_query):
     difference = 2 if stored_query else 1
-    return upcoming_operator_and_index and len(query_stack) - index == difference and operator_token.type in [
+    return upcoming_operator_and_index and len(query_stack) - index == difference and operator_token.type in (
         TokenType.UNION,
         TokenType.INTERSECTION,
-        TokenType.DIFFERENCE]
+        TokenType.DIFFERENCE)
 
 
-def get_upcoming_operator_token(query_stack):
+def get_upcoming_operator_token(query_stack, is_binary=None):
+    operator = TOKEN_TYPE_OPERATORS
+    if is_binary is not None:
+        operator = TOKEN_TYPE_TO_BINARY_OPERATOR if is_binary else TOKEN_TYPE_TO_UNARY_OPERATOR
     for index in reversed(range(len(query_stack))):
-        if query_stack[index][-1].type in TOKEN_TYPE_OPERATORS:
+        if query_stack[index][-1].type in operator:
             return query_stack[index][-1], index
+    return None, None
 
 
 def get_level(query_stack, level):
