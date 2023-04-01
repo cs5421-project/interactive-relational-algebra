4216d5f7d59f66a0fae0f9802f735a83cbc6f03b
vijayganesh <vijayganesh517@gmail.com>
2023-04-01 09:37:09 +0800

    format document

--- backend/ira/service/raq_converter.py
+++ backend/ira/service/raq_converter.py
@@ -1,28 +1,30 @@
 import xml.etree.ElementTree as ET
 
-def raq_converter(XML:str):
+
+def raq_converter(XML: str):
     try:
         root = ET.fromstring(XML)
     except ET.ParseError as error:
-        raise Exception(error.msg)
+        raise error
 
     return parse(root)
 
+
 def parse(node):
     value = ""
-        
-    if len(node) == 0: 
+
+    if len(node) == 0:
         return node.text.strip()
-    
+
     for c in node:
-        preV =  postV = ""
-        
+        preV = postV = ""
+
         if c.tag == "parenthesis":
             preV, postV = "(", ")"
-        
+
         if c.tag == "binary_operator":
             preV = postV = " "
-        
+
         value += preV + parse(c) + postV
-    
+
     return value
